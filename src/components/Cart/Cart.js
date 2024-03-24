a677d5274fccf56dde445b023e7dad42f6cfd78e
Debasish-1903 <das31debasish@gmail.com>
2024-03-24 20:19:20 +0530

    cart.js updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,25 @@
 import Modal from "../UI/Modal";
+import "./Cart.css";
 
-function Cart({ showCart, closeCart }) {
+function CartItem({ id, name, image, quantity }) {
+  return <div className="cart-item">{name}</div>;
+}
+
+function Cart({ showCart, closeCart, cartItems }) {
   return (
     <Modal show={showCart} onclose={closeCart}>
-      Hellow Buddy
+      <div className="cart-container">
+        <div className="cart-heading">Cart</div>
+        {cartItems.map((item) => (
+          <CartItem
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            quantity={item.quantity}
+          />
+        ))}
+      </div>
     </Modal>
   );
 }
