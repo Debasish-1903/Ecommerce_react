1840b9287bf931b941e0c0220b05275974f7bab6
Debasish-1903 <das31debasish@gmail.com>
2024-03-24 14:48:46 +0530

    eventpropagation stopped on modal in Modal.js

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -5,9 +5,23 @@ function Modal({ show, onclose, children }) {
     return null;
   }
 
+  // const demo = (event) => {
+  //   console.log(event.target);
+  // };
+
   return (
+    // <div className="backdrop" onClick={demo}>
     <div className="backdrop" onClick={onclose}>
-      <div className="modal">{children}</div>
+      <div
+        className="modal"
+        onClick={(event) => {
+          // when eventlistener of parent passes in children then it is called
+          //  eventpropagation it is a concept of valina js that's why we need to stop it
+          event.stopPropagation();
+        }}
+      >
+        {children}
+      </div>
     </div>
   );
 }
