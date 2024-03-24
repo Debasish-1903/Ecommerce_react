ccd9e080778db7b8b44515913d411911d4c7ef4f
Debasish-1903 <das31debasish@gmail.com>
2024-03-24 20:18:32 +0530

    cartItem added as state

--- src/App.js
+++ src/App.js
@@ -8,15 +8,37 @@ import Cart from "./components/Cart/Cart.js";
 function App() {
   const [showCart, setShowCart] = useState(false);
 
+  const [cartItems, setCartItems] = useState([]);
+
   const openCart = () => setShowCart(true);
 
   const closeCart = () => setShowCart(false);
 
+  const handleAddToCart = (productId, productName, productImage) => {
+    // let updatedCartItems = cartItems;
+    // updatedCartItems = updatedCartItems.concat({   //pusing without changing the original array
+    //   id: productId,
+    //   name: productName,
+    //   image: productImage,
+    //   quantity: 1,
+    // });
+
+    //using alternatively useing spread operator in one line
+
+    const cartItem = {
+      id: productId,
+      name: productName,
+      image: productImage,
+      quantity: 1,
+    };
+    setCartItems((state) => [...state, cartItem]);
+  };
+
   return (
     <div>
       <Header openCart={openCart} />
-      <Products />
-      <Cart showCart={showCart} closeCart={closeCart} />
+      <Products onAddToCart={handleAddToCart} />
+      <Cart showCart={showCart} closeCart={closeCart} cartItems={cartItems} />
     </div>
   );
 }
