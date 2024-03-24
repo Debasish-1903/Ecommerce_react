cd30e9654bb4b77099bda54384a137818aa9ef48
Debasish-1903 <das31debasish@gmail.com>
2024-03-24 14:28:21 +0530

    modified structuring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
 import Header from "./components/Header/Header";
 import Products from "./components/Products/products";
+import Cart from "./components/Cart/Cart.js";
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const openCart = () => setShowCart(true);
+
+  const closeCart = () => setShowCart(false);
+
   return (
     <div>
-      <Header />
+      <Header openCart={openCart} />
       <Products />
+      <Cart showCart={showCart} closeCart={closeCart} />
     </div>
   );
 }
