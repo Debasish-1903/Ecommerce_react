09154235a76890a3f9f71e164b1c678686ab245d
Debasish-1903 <das31debasish@gmail.com>
2024-03-24 14:29:08 +0530

    modifed header file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,6 @@
-import { useState } from "react";
 import "./Header.css";
-import Modal from "../UI/Modal";
-function Header() {
-  const [showCart, setShowCart] = useState(false);
 
-  function openCart() {
-    setShowCart(true);
-  }
-
-  function closeCart() {
-    setShowCart(false);
-  }
-
+function Header({ openCart }) {
   return (
     <div className="header">
       <h1>My React store</h1>
@@ -20,18 +9,6 @@ function Header() {
           Cart
         </button>
       </div>
-
-      {/* making better--reusable component */}
-      <Modal show={showCart} onclose={closeCart}>
-        Hellow Buddy
-      </Modal>
-
-      {/* //conditional rendering of state */}
-      {/* {showCart && (
-        <div className="backdrop" onClick={closeCart}>
-          <div className="modal">Hello form cart</div>
-        </div>
-      )} */}
     </div>
   );
 }
