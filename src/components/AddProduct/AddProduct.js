f2859a59afcc2eda5a51daf37a687b8f799dc7bf
Debasish-1903 <das31debasish@gmail.com>
2024-04-01 00:29:04 +0530

    form handeling done

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,14 +1,22 @@
 import Modal from "../UI/Modal";
 import "./AddProduct.css";
+import { useRef } from "react";
 
-function AddProduct({ showAddProduct, closeAddProduct }) {
+function AddProduct({ showAddProduct, closeAddProduct, onAddProduct }) {
+  const nameRef = useRef();
+  function handlesubmit(event) {
+    event.preventDefault();
+    const nameValue = nameRef.current.value;
+    onAddProduct(nameValue);
+  }
+
   return (
     <Modal show={showAddProduct} onClose={closeAddProduct}>
       <div className="add-product-container">
         <div className="add-product-heading">AddProduct</div>
-        <form onSubmit={() => {}} className="add-product-form">
+        <form onSubmit={handlesubmit} className="add-product-form">
           <div className="form-label">Enter Product Name</div>
-          <input className="form-input" />
+          <input className="form-input" name="Product name" ref={nameRef} />
           <button type="submit" className="yellow-button submit-button">
             Add product
           </button>
